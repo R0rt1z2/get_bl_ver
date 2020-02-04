cc708b80c1d0e3f2d7db018114113a3bca336458
Roger Ortiz <39171306+R0rt1z2@users.noreply.github.com>
2020-02-04 21:04:09 +0100

    Improvements

--- get_bl_ver.py
+++ get_bl_ver.py
@@ -1,15 +1,24 @@
 import sys
 
 global compare
-VER_STRING = b'LK_VER:'
+STR_VER = b'LK_VER:'
 
 def main():
   if compare:
     print("I: Comparing two bootloaders...")
+    if sys.argv[2] == sys.argv[3]:
+        print("E: You cannot compare same bootloader! Abort...")
+        exit(1)
     data1 = bl1.read()
     data2 = bl2.read()
-    offset1 = data1.find(VER_STRING)
-    offset2 = data2.find(VER_STRING)
+    if b'AMZN' not in data1:
+       print("E: Bootloader 1 is not from Amazon! Abort...")
+       exit(1)
+    elif b'AMZN' not in data2:
+       print("E: Bootloader 2 is not from Amazon! Abort...")
+       exit(1)
+    offset1 = data1.find(STR_VER)
+    offset2 = data2.find(STR_VER)
     bl1.seek(offset1 + 7)
     bl2.seek(offset2 + 7)
     ver1 = bl1.read(2)
@@ -24,13 +33,16 @@ def main():
       print("I: Bootloaders have the same version: {} ({})".format(ver1, int_ver1))
   else:
     data = bl.read()
-    offset = data.find(VER_STRING)
+    if b'AMZN' not in data:
+        print("E: Bootloader is not from Amazon! Abort...")
+        exit(1)
+    offset = data.find(STR_VER)
     bl.seek(offset + 7)
     ver = bl.read(2) # 2 bytes determine ver
     int_ver = int.from_bytes(ver, "little")
     ver = ver.hex()
     print("I: BL Version: {} ({})".format(ver, int_ver))
-  
+
 if __name__ == "__main__":
   if sys.argv[1] == "-c":
      compare = True
